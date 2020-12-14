1b7caefdd03a720deac4edd8a31948422308ef5d
Makita14 <63524887+Makita14@users.noreply.github.com>
2020-12-14 19:58:58 -0300

    Add files via upload

--- Clase_3_Foro1/src/Escuela.java
+++ Clase_3_Foro1/src/Escuela.java
@@ -7,12 +7,28 @@ public class Escuela {
 		this.nombre=nombre;
 		this.nivel=nivel;
 	}
+	
+	public void setNombre(String nombre) {
+		this.nombre=nombre;
+	}
+	
+	public void setNivel(String nivel) {
+		this.nivel=nivel;
+	}
+	
 	public String getNombre() {
 		return this.nombre;
 	}
+	
 	public String getNivel() {
 		return this.nivel;
 	}
+	
+	public boolean equals(Object o){
+        Escuela escuela = (Escuela) o;
+        return (escuela.nombre == nombre) && (escuela.nivel == nivel);
+    }
+	
 	public String toString() {
 		return "Nombre: "+nombre +" Nivel: "+nivel;
 	}
--- Clase_3_Foro1/src/Principal.java
+++ Clase_3_Foro1/src/Principal.java
@@ -1,6 +1,8 @@
-
 public class Principal {
 	public static void main (String[] args) {
+		
+		boolean M;
+		
 		Escuela unaEscuela = new Escuela ("Instituto River Plate","Secundaria");
 		System.out.println(unaEscuela);
 		System.out.println("-------------------------------------------------");
@@ -9,11 +11,20 @@ public class Principal {
 		System.out.println(unGrado);
 		System.out.println("-------------------------------------------------");
 		
+		//Casting escuela
 		Escuela otraEscuela = (Escuela) unGrado;
 		System.out.println (otraEscuela);
 		System.out.println("-------------------------------------------------");
 		
-		Grado otroGrado = (Grado) unaEscuela;
-		System.out.println (otroGrado);
+		//Casting grado
+		//Grado otroGrado = (Grado) unaEscuela;
+		//System.out.println (otroGrado);
+		
+		//Equals
+        Escuela escuela_1 = new Escuela("Alicia Moreau de Justo", "Secundaria");
+        Escuela escuela_2 = new Escuela("Alicia Moreau de Justo", "Secundaria");
+        
+        M = escuela_1.equals(escuela_2);
+        System.out.println(M);
 	}
 }
