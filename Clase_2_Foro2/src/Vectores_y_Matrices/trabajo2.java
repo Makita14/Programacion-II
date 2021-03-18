bc79371bb6072f4577ffb08808d514e6af1bd19f
Makita14 <63524887+Makita14@users.noreply.github.com>
2021-03-18 20:53:49 -0300

    Add files via upload

--- Clase_2_Foro2/src/Vectores_y_Matrices/trabajo2.java
+++ Clase_2_Foro2/src/Vectores_y_Matrices/trabajo2.java
@@ -1,16 +1,63 @@
 package Vectores_y_Matrices;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class trabajo2 {
 public  static  void  main ( String [] args ) {
 	
-		Scanner leer = new Scanner ( System . in);
-		String nombreArch= "[i] user + [i] pass";
-		String nombreTrab = "Loginusua.txt";
-		gestorArchivoTrab2.guardar(nombreTrab,nombreArch);
+		//Scanner leer = new Scanner ( System . in);
 		
-		String [] user = new String [ 4 ];
+	/*String nombreArch= "[i] user + [i] pass";
+		String nombreTrab = "LoginYsua.txt";
+		gestorArchivoTrab2.guardar(nombreTrab,nombreArch);*/
+		
+		
+		ArrayList<String> empausua = new ArrayList<>();
+		empausua.add("Maca");
+		empausua.add("Makarenita");
+		empausua.add("Yael");
+		empausua.add("Macarena");
+		
+		ArrayList<String> empapass = new ArrayList<>();
+		empapass.add("91218");
+		empapass.add("91216");
+		empapass.add("91215");
+		empapass.add("91214");
+		
+		String separador = "*";
+		
+		String paquete = gestorArchivoTrab2.empaquetador(empausua, separador);
+		
+		System.out.println(paquete);
+		
+		gestorArchivoTrab2.guardar("loginusua.txt", paquete);
+		
+		
+		String separa="*";
+		
+		String paquetepass = gestorArchivoTrab2.empaquetador(empapass, separa);
+		
+		System.out.println(paquetepass);
+		
+		gestorArchivoTrab2.guardar("loginpass.txt", paquetepass);
+		
+		/*
+		
+		String paquete = gestorArchivoTrab2.cargar("loginusua.txt");
+		
+		System.out.println(paquete);
+		
+		String separador2 = "*";
+		
+		ArrayList<String> loginusua = gestorArchivoTrab2.desempaquetador(paquete, separador2);
+		
+		for(int j=0;j<loginusua.size();j++) {
+			System.out.println("--> " + loginusua.get(j));
+		}
+		
+		
+		/*String [] user = new String [ 4 ];
 				user [ 0 ] =  " Daiana " ;
 				user [ 1 ] =  " Facundo" ;
 				user [ 2 ] =  " Osvaldo " ;
@@ -22,7 +69,9 @@ public  static  void  main ( String [] args ) {
 		   		pass [ 2 ] =  1122 ;
 		        pass [ 3 ] =  3344 ;
 		
-        System.out.println  ( " Ingrese su nombre de usuario: " );		
+		        
+		        
+      /*  System.out.println  ( " Ingrese su nombre de usuario: " );		
         String usuario = leer .next();		
         System.out.println ( " Ingrese su contraseña: " );		
         int contrasena = leer . nextInt ();
@@ -44,5 +93,5 @@ public  static  void  main ( String [] args ) {
 	        System.out.println( " Su contraseña ha sido cambiada con exito " );
 	        System.out.println( " Su nueva contraseña es: "  + con);
 	        System.out.println ( "    Bienvenido/a, usted se ha registrado correctamente " );
-		}
+		*/}
 }
